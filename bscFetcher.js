b0117532c4edbf371a6c1affb3232a8612dcbd82
Ami Hazbany <ami@orbs.com>
2021-08-03 13:52:53 +0300

    fetchListedPairs, getPastEventsLoop opt

--- bscFetcher.js
+++ bscFetcher.js
@@ -8,11 +8,12 @@ async function getPastEventsLoop(contract, eventName, nBlocks, endBlock, chunkSi
 	let _toBlock = endBlock
 	let _fromBlock= Math.max(endBlock-nBlocks+1, endBlock-chunkSize+1);
 	const exitBlock = endBlock - nBlocks + 1
+	let retry = 0
 
 	while (true) {
 
 		try {
-
+			console.log(`fromBlock=${_fromBlock}, toBlock=${_toBlock}`)
 			events = events.concat(await contract.getPastEvents(eventName, {filter: filterObj, fromBlock: _fromBlock, toBlock: _toBlock}))
 
 			console.log(events)
@@ -24,11 +25,19 @@ async function getPastEventsLoop(contract, eventName, nBlocks, endBlock, chunkSi
 				console.log(`${e}: setting chunkSize to ${_chunkSize}`)
 			}
 			else {
-				console.log(`unhandled error: ${e.message}`)
+				if (retry < 5) {
+					retry += 1
+					console.log(`[ERROR] failed to fetch data: ${e.message}, retry ${retry} ...`)
+					continue
+
+				} else {
+					console.log(`[ERROR] failed to fetch data: ${e.message}, skipping range fromBlock=${_fromBlock} toBlock=${_toBlock} ...`)
+				}
 			}
 
 		}
 
+		retry = 0
 		_toBlock = _fromBlock - 1
 		_fromBlock = Math.max(exitBlock, _fromBlock-_chunkSize)
 		_chunkSize = chunkSize
--- strategy/strategy.js
+++ strategy/strategy.js
@@ -1,7 +1,7 @@
 const Web3 = require('web3');
 const BigNumber = require('bignumber.js');
 const {SMARTCHEF_FACTORY_ADDRESS, CAKE_ADDRESS, PANCAKESWAP_FACTORY_V2_ADDRESS, VERSION} = require('./params');
-const {SMARTCHEF_FACTORY_ABI, CAKE_ABI} = require('../abis');
+const {SMARTCHEF_FACTORY_ABI, CAKE_ABI, PANCAKESWAP_FACTORY_V2_ABI, BEP_20_ABI} = require('../abis');
 
 require('dotenv').config();
 const {TransactionFailure, FatalError, GasError, NotImplementedError} =  require('../errors');
@@ -46,7 +46,7 @@ class Strategy extends TxManager {
 	BLOCKS_PER_DAY = this.SECONDS_PER_DAY / this.AVG_BLOCK_SEC;
 	BLOCKS_PER_YEAR = this.BLOCKS_PER_DAY * 365;
 
-	PAST_EVENTS_N_DAYS = 1;
+	PAST_EVENTS_N_DAYS = 3;
 	PAST_EVENTS_N_BLOCKS = Math.floor(this.PAST_EVENTS_N_DAYS * this.BLOCKS_PER_DAY);
 
 	constructor() {
@@ -121,6 +121,7 @@ class Strategy extends TxManager {
 		this.cakeContract = await this.getContract(CAKE_ABI, CAKE_ADDRESS)
 		this.swapFactoryContract = await this.getContract(PANCAKESWAP_FACTORY_V2_ABI, PANCAKESWAP_FACTORY_V2_ADDRESS)
 
+		await this.fetchListedPairs()
 		await this.fetchPools()
 	}
 
@@ -134,60 +135,30 @@ class Strategy extends TxManager {
 
 	}
 
-	async fetchListedPairs(blockNum=null, fetchNBlocks=this.PAST_EVENTS_N_BLOCKS) {
+	async fetchListedPairs(filterList, blockNum=null, fetchNBlocks=this.PAST_EVENTS_N_BLOCKS) {
 
 		// TODO: fetch from redis last block and fetch only last missing blocks
+
 		if (blockNum === null) {
 			blockNum = await web3.eth.getBlockNumber()
 		}
 
-		// TODO: store on redis
-		let events = await getPastEventsLoop(this.swapFactoryContract, 'PairCreated', fetchNBlocks, blockNum)
-		// let events = await getPastEventsLoop(this.smartchefFactoryContract, 'NewSmartChefContract', 1, 9676518) // 9676510, TODO : remove me dbg only
+		// let event = await this.swapFactoryContract.getPastEvents('PairCreated', {filter: {'token0': '0x6c8359e75b97471cabdd571a44ca499571d31f89'}, fromBlock: blockNum-1000, toBlock: blockNum})
+		// debug(event)
 
-		let symbol
-
-		for (const event of events) {
-			let poolAddr = event['returnValues']['smartChef']
-			let abi = await this.fetchAbi(poolAddr)
-			let contract = this.getContract(abi, poolAddr)
-			let rewardToken = await contract.methods.rewardToken().call()
-			let stakedToken = await contract.methods.stakedToken().call()
-			let hasUserLimit = await contract.methods.hasUserLimit().call()
-			let rewardPerBlock = await contract.methods.rewardPerBlock().call()
-
-			debug(`rewardToken=${rewardToken}, stakedToken=${stakedToken}, hasUserLimit=${hasUserLimit}, ${hasUserLimit===true}`)
-
-			if (stakedToken !== CAKE_ADDRESS) {
-				continue
-			}
-
-			try {
-				contract = this.getContract(await this.fetchAbi(rewardToken), rewardToken)
-				symbol = await contract.methods.symbol().call()
-
-			} catch (e) {
-				debug(`failed to fetch rewardToken (${rewardToken}) symbol: ${e}`)
-				continue
-			}
-
-			debug(symbol, rewardToken, stakedToken)
-
-			this.poolsInfo[poolAddr] = {'rewardToken': rewardToken, 'rewardSymbol': symbol, 'hasUserLimit': hasUserLimit, 'rewardPerBlock': rewardPerBlock, 'abi': abi}
-			// TODO: store redis
-
-		}
-
-		const await this.swapFactoryContract.methods.balanceOf(addr)
+		// // TODO: store on redis
+		let events = await getPastEventsLoop(this.swapFactoryContract, 'PairCreated', fetchNBlocks, blockNum, 5000, {'token0': filterList})
+		events.concat(await getPastEventsLoop(this.swapFactoryContract, 'PairCreated', fetchNBlocks, blockNum, 5000, {'token1': filterList}))
 
 	}
 
 	async fetchPools(blockNum=null, fetchNBlocks=this.PAST_EVENTS_N_BLOCKS) {
 
-		// TODO: fetch from redis last block and fetch only last missing blocks
 		if (blockNum === null) {
 			blockNum = await web3.eth.getBlockNumber()
 		}
+
+		// TODO: fetch from redis last block and fetch only last missing blocks
 
 		// TODO: store on redis
 		let events = await getPastEventsLoop(this.smartchefFactoryContract, 'NewSmartChefContract', fetchNBlocks, blockNum)
@@ -215,8 +186,11 @@ class Strategy extends TxManager {
 				symbol = await contract.methods.symbol().call()
 
 			} catch (e) {
-				debug(`failed to fetch rewardToken (${rewardToken}) symbol: ${e}`)
-				continue
+				this.notif.sendDiscord(`failed to fetch rewardToken (${rewardToken}): ${e}, trying fetch with bep-20 abi ...`)
+
+				contract = this.getContract(BEP_20_ABI, rewardToken)
+				symbol = await contract.methods.symbol().call()
+				this.notif.sendDiscord(`succeeded fetching (${rewardToken}) info`)
 			}
 
 			debug(symbol, rewardToken, stakedToken)
