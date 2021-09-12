d49de7fc6de6e61a14eed6405a4bf0c49890ac6d
Ami Hazbany <ami@orbs.com>
2021-09-12 20:21:34 +0300

    remove exit

--- strategy/strategy.js
+++ strategy/strategy.js
@@ -111,8 +111,8 @@ class Strategy {
     async start() {
         try {
         	logger.debug(`[Strategy] start`)
-			await this.reporter.send({apy: 10})
-			process.exit()
+			// await this.reporter.send({apy: 10})
+			// process.exit()
 
 	        this.lastActionTimestamp = await this.getLastActionTimestamp();
 
