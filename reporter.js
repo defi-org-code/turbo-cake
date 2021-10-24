71d568ad84d20960a32481ed6a6ab2f930dbaebd
Ami Hazbany <ami@orbs.com>
2021-10-24 16:37:46 +0300

    reporter chage back to influx

--- reporter.js
+++ reporter.js
@@ -1,39 +1,17 @@
-const graphite = require('graphite');
-const {VERSION, GRAPHITE_IP} = require('./core/params')
-require('dotenv').config();
+const Influx = require('./influx');
+const {VERSION} = require('./core/params')
+const env = require('dotenv').config();
 
-const {Logger} = require('./logger')
-const logger = new Logger('reporter')
 
 class Reporter {
 
 	constructor(runningMode) {
-		// this.graphiteClient = graphite.createClient(`plaintext://${GRAPHITE_IP}:2003`)
-		this.runningMode = runningMode
-		this.prefix = `turbo-cake.${this.runningMode}.BOT-ID=${process.env.BOT_ID}.${VERSION}`
+		this.influxClient = new Influx('TurboCake', VERSION);
+		this.prefix = `${runningMode}.${process.env.BOT_ID}.${VERSION}`
 	}
 
-	addPrefix(_metrics, prefix) {
-
-		let metrics = {}
-
-		for (const key of Object.keys(_metrics)) {
-			metrics[`${prefix}.${key}`] = _metrics[key]
-		}
-
-		return metrics
-	}
-
-	send(metrics) {
-		const graphiteClient = graphite.createClient(`plaintext://${GRAPHITE_IP}:2003`)
-		graphiteClient.write(this.addPrefix(metrics, this.prefix), Date.now(), function(err) {
-		  // if err is null, your data was sent to graphite!
-		  if (err !== null) {
-			  console.log(`error sending data to graphite ${err}`)
-		  }
-		});
-
-		graphiteClient.end()
+	async send(fields, tags={}) {
+		this.influxClient.report(this.prefix, fields, tags)
 	}
 }
 
